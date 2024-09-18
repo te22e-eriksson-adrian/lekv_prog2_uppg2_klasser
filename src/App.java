6fdd8fd9c989b42d594f0dcc9cbaf96f4e6424d5
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-09-18 11:06:04 +0200

    Skapade grund för projektet

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Player p1 = new Player("Belerothon", 50);
+        Enemy p1 = new Enemy("Caimera", 40);
+    }
+}
